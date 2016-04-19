a73c520d2291d6324498c227cf0b9ec3de28ce6c
sherylynn <352281674@qq.com>
2016-04-20 07:52:15 +0800

    111
    111111

--- views/me/resume.js
+++ views/me/resume.js
@@ -75,8 +75,18 @@ export default class Resume extends Component {
                     </TouchableOpacity>
                     <Text style={styles.caption_text}>{this.props.title}</Text>
                 </View>
-                <View style={{ padding: 15 }}>
-                    <Text>{this.props.title} 等待添加</Text>
+                <View style={styles.container}>
+                <View style={styles.inputRow}>
+                    <Text>邮箱</Text><TextInput style={styles.input} placeholder="请输入邮箱" onChangeText={this._getEmail}/>
+                </View>
+                <View style={styles.inputRow}>
+                    <Text>密码</Text><TextInput style={styles.input} placeholder="请输入密码" password={true} onChangeText={this._getPassword}/>
+                </View>
+                <View>
+                    <TouchableHighlight underlayColor="#fff" style={styles.btn} onPress={this._login}>
+                        <Text style={{ color: '#fff' }}>登录</Text>
+                    </TouchableHighlight>
+                </View>
                 </View>
             </View>
         );
@@ -89,7 +99,7 @@ export default class Resume extends Component {
                     <TouchableOpacity onPress={() => this._pressButton() }>
                         <Image source={require('../../images/icon_back.png') } style={styles.back_img}/>
                     </TouchableOpacity>
-                    
+
                         <View style={styles.container}>
                             <View>
                                 <Image source={require('../../images/avatar.png') } style={styles.avatar}/>
@@ -113,7 +123,7 @@ export default class Resume extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        paddingTop:15,
         alignItems: 'center',
         backgroundColor: '#F5FCFF'
     },
@@ -150,7 +160,7 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         width: 220,
         borderWidth: Util.pixel,
-        height: 35,
+        height: 40,
         paddingLeft: 8,
         borderRadius: 5,
         borderColor: '#ccc'
